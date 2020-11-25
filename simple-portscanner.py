c424c2722dec29e33aab19ecf5a52ba93e375705
Zeuse-coder <vyinghope90@gmail.com>
2020-11-25 14:54:34 +0530

    This is a scanner that scans ports upto 8080(you can change it in the scripts).It also shows server banner of the open ports..make this scanner usefull

--- simple-portscanner.py
+++ simple-portscanner.py
@@ -7,7 +7,7 @@ import sys
 from datetime import datetime
 import threading
 import colorama
-from colorama import Fore
+from colorama import *
 colorama.init()
 subprocess.call('clear',shell=True)
 
@@ -18,20 +18,31 @@ print ("Please wait, scanning remote host ",ip)
 print (Fore.YELLOW + "-"*60+Fore.WHITE)
 
 t1=datetime.now()
+def retBanner(ip,port):
+  try:
+      s=socket.socket()
+      s.connect((ip,port))
+      banner=s.recv(4069)
+      return banner
+  except:
+      pass
 def scan(ip,port):
  try:
        s=socket.getservbyport(port)
        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
        result=sock.connect_ex((ip, port))
+       banner=retBanner(ip,port)
        if result == 0:
-           print(Fore.WHITE+"port" f'[{port}]:'+ Fore.GREEN+ "open   service="f'[{s}]' .format(port,s))
-           #print("port %d :" + Fore.GREEN + " Open " %(port))
-           print(Style.RESET)
+        print(Fore.WHITE+"port" f'[{port}]:'+ Fore.GREEN+"open   Services="f'[{s}] \n'+Fore.BLUE +' \t\t BANNER= 'f'[{ banner}]'.format(port,s,banner))
+        
+        print(Style.RESET)
+        print(" \n ")
+        print("\n")
        sock.close()  
  except socket.gaierror:
      print( Fore.RED + "couldn't connect to server")
      sys.exit()
-
+     
 with concurrent.futures.ThreadPoolExecutor(max_workers=250) as executor:
     for port in range(1,8080):  
       executor.submit(scan,ip,port)
@@ -40,5 +51,5 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=250) as executor:
 t2= datetime.now()
 total= t2-t1
 print ("Scanning completed in :",total)
-print (" Use me often :)")
+print (" Use me often 👾😊")
 
